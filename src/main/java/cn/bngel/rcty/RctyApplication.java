b7d30e9ffff49ed297115fde8f15235813bd9e76
bngel <bngel1024@gmail.com>
2021-11-22 22:23:59 +0800

    feat: controller finished

--- src/main/java/cn/bngel/rcty/RctyApplication.java
+++ src/main/java/cn/bngel/rcty/RctyApplication.java
@@ -1,9 +1,11 @@
 package cn.bngel.rcty;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.bngel.rcty.dao")
 public class RctyApplication {
 
     public static void main(String[] args) {
